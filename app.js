7fc8276f47a1a395e30ebefcabd55b18106d0287
Rakesh S1 <rakesh.s1@mphasis.com>
2020-12-15 14:21:24 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const signinbtn = document.querySelector("#sign-in-btn");
+const signupbtn = document.querySelector("#sign-up-btn");
+const contanier = document.querySelector(".container");
+
+signupbtn.addEventListener('click', () => {
+    contanier.classList.add("sign-up-mode");
+})
+
+signinbtn.addEventListener('click', () => {
+    contanier.classList.remove("sign-up-mode");
+})
